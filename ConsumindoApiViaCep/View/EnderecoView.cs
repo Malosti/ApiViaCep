5fbc5663a532ad7633432553c94f1318a4753aaf
Guilherme Malosti Fonseca <guilherme.fonseca@gsw.com.br>
2023-02-27 15:36:06 -0300

    Exclusao bibliotecas nao utilizadas

--- ConsumindoApiViaCep/View/EnderecoView.cs
+++ ConsumindoApiViaCep/View/EnderecoView.cs
@@ -1,9 +1,4 @@
 ﻿using ConsumindoApiViaCep.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsumindoApiViaCep.View
 {
