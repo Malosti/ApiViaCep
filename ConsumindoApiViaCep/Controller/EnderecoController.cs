4789746d15845243ce8b1e10fb0c13a955107cb0
Guilherme Malosti Fonseca <guilherme.fonseca@gsw.com.br>
2023-02-27 16:38:29 -0300

    Implementada a classe EnderecoController

---
+++ ConsumindoApiViaCep/Controller/EnderecoController.cs
@@ -0,0 +1,39 @@
+﻿using ConsumindoApiViaCep.Entities;
+using ConsumindoApiViaCep.Services;
+using ConsumindoApiViaCep.View;
+using System.Text.RegularExpressions;
+
+namespace ConsumindoApiViaCep.Controller
+{
+    internal class EnderecoController
+    {
+        public async Task BuscadorCepAsync()
+        {
+        inicio:
+
+            var enderecoService = new EnderecoService();
+
+            EnderecoView.TelaInicial();
+            var cep = Console.ReadLine();
+            Regex cepValido = new Regex("^[0-9]{8}$", RegexOptions.None);
+
+
+            if (!cepValido.IsMatch(cep))
+            {
+                EnderecoView.TelaCPFInvalido();
+                goto inicio;
+            }
+
+            Endereco enderecoLocalizado = await enderecoService.ConsultaEndereco(cep);
+
+            if (enderecoLocalizado.erro == true)
+            {
+                Console.WriteLine("CEP não localizado");
+            }
+            else if (enderecoLocalizado.erro == false)
+            {
+                EnderecoView.TelaEndereco(enderecoLocalizado);
+            }
+        }
+    }
+}
--- ConsumindoApiViaCep/Program.cs
+++ ConsumindoApiViaCep/Program.cs
@@ -1,33 +1,9 @@
-﻿using ConsumindoApiViaCep.Entities;
-using ConsumindoApiViaCep.Services;
-using ConsumindoApiViaCep.View;
-using System.Text.RegularExpressions;
+﻿
+using ConsumindoApiViaCep.Controller;
 
-inicio:
+var enderecoController = new EnderecoController();
 
-var enderecoService = new EnderecoService();
-
-EnderecoView.TelaInicial();
-var cep = Console.ReadLine();
-Regex cepValido = new Regex("^[0-9]{8}$", RegexOptions.None);
-
-
-if (!cepValido.IsMatch(cep))
-{
-    EnderecoView.TelaCPFInvalido();
-    goto inicio;
-}
-
-Endereco enderecoLocalizado = await enderecoService.ConsultaEndereco(cep);
-
-if (enderecoLocalizado.erro == true)
-{
-    Console.WriteLine("CEP não localizado");
-}
-else if (enderecoLocalizado.erro == false)
-{
-    EnderecoView.TelaEndereco(enderecoLocalizado);
-}
+await enderecoController.BuscadorCepAsync();
 
 
 
